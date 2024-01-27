7a70ac54a977a90e5bc5ba7661b85e34ef017549
apalrd <apalrd@gmail.com>
2024-01-27 23:48:24 +0000

    Copyright

--- nat64.c
+++ nat64.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
-/* Copyright (c) 2022 Hengqi Chen */
+/* Copyright (c) 2024 Andrew Palardy */
 #include <signal.h>
 #include <arpa/inet.h>
 #include <unistd.h>
